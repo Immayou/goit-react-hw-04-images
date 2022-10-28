09150bfb39e2c30b6c343faa930ccdf8a6509c0e
Yuliya Zozulya <djulia@ukr.net>
2022-10-28 12:25:46 +0300

    refactoring modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types'; 
 import { createPortal} from 'react-dom';
 import { Overlay, ModalWindow } from "../Modal/Modal.styled"; 
 
+
 const modalRoot = document.querySelector('#modal-root')
 
 export const Modal = ({onModalClose, children}) => {
-  
-   const handleKeydown = e => {
+    
+  useEffect (() => {   
+    const handleKeydown = e => {
       if (e.code === 'Escape') {
-        console.log(1)
         onModalClose()
       }
-    }
+    }  
+    window.addEventListener('keydown', handleKeydown)
+    return () => {window.removeEventListener('keydown', handleKeydown)}
+    })
 
-  window.addEventListener('keydown', handleKeydown)
-//   window.removeEventListener('keydown', handleKeydown)
 
   const handleBackdropClick = e => {
         if (e.currentTarget === e.target) {
